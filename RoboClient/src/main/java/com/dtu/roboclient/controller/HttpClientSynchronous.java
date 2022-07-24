94370b2d7596229eb20667940fc7baf6131deceb
EspenUlff <Espen.larp@gmail.com>
2022-07-24 13:17:15 +0200

    GetGame implemented

--- RoboClient/src/main/java/com/dtu/roboclient/controller/HttpClientSynchronous.java
+++ RoboClient/src/main/java/com/dtu/roboclient/controller/HttpClientSynchronous.java
@@ -1,5 +1,8 @@
 package com.dtu.roboclient.controller;
 
+import com.dtu.common.model.Board;
+import com.dtu.common.model.fileaccess.LoadBoard;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -17,7 +20,8 @@ public class HttpClientSynchronous {
             .build();
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(NewGame(2));
+        //System.out.println(NewGame(2));
+        //System.out.println(GetGame(UUID.fromString("9b1c7d0d-bce4-4483-9ffc-7d9805ee312d")));
     }
 
     public static UUID NewGame(int players) throws IOException, InterruptedException {
@@ -27,10 +31,20 @@ public class HttpClientSynchronous {
                 .build();
         var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         var body = response.body();
-        //System.out.println(body);
-        //System.out.println(UUID.randomUUID().toString());
+        /*System.out.println(body);
+        System.out.println(UUID.randomUUID().toString());*/
         return UUID.fromString(body.replace("\"", ""));
     }
 
+    public static Board GetGame(UUID id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(BaseURI + "/game/" + id))
+                .build();
+        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        var body = response.body();
+        System.out.println(body);
+        return LoadBoard.loadBoard(body);
+    }
 }
 
