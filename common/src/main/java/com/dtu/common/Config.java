d9fce92c821ad2d0f47beb38c60a5b82bc3c8eb6
EspenUlff <Espen.larp@gmail.com>
2022-07-24 13:22:55 +0200

    Added board height/width to config file

--- common/src/main/java/com/dtu/common/Config.java
+++ common/src/main/java/com/dtu/common/Config.java
@@ -6,4 +6,7 @@ import java.util.List;
 public class Config {
     public final static List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
     public final static String[] PLAYER_COLORS = {"red", "green", "blue", "orange", "grey", "magenta"};
+
+    public final static int DEFAULT_BOARD_HEIGHT = 8;
+    public final static int DEFAULT_BOARD_WIDTH = 8;
 }
